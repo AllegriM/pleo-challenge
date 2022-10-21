ee712276fca87f3152eb6cf6f04c44aa30295c6f
AllegriM <elmonky99@gmail.com>
2022-10-21 18:38:30 -0300

    feat: :sparkles: Home finished & react router
    Can navigate to launches & launch pads

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Heading, Stack, Text, VStack, HStack } from '@chakra-ui/react'
+import { Routes, Route, Link } from "react-router-dom";
+import ArrowRight from './Components/Icons/Arrow';
+
+
+const Home: React.FC = () => {
+  
+  const SECTIONS = [{id: "launches", title: "Browse SpaceX Launches"}, {id: "launch-pads", title: "Browse SpaceX Launch Pads"}]
+  
+  return(
+    <Stack bg={"white"} w={"95%"} h={"100%"} pt={"1rem"} gap={4}>
+      {
+        SECTIONS.map(section => {
+          return(
+            <Link key={section.id} to={section.id}>
+              <HStack _hover={{ textDecoration: "underline"}} p={6} justify={"space-between"} borderRadius={8} border={"1px solid #e9e9e9"} boxShadow={"lg"} cursor={"pointer"} w={"100%"}>
+                <Heading as='h4' size='md' fontWeight={"normal"}>{section.title}</Heading>
+                <ArrowRight />
+              </HStack>
+            </Link>
+          )
+        })
+      }
+    </Stack>
+  )
+}
+
+const Navbar: React.FC = () =>  {
+  return (
+    <Stack className='navbar' bg="gray.800" w={"100%"} textColor={"white"} p={6}>
+      <Heading  as='h4' size='md' fontFamily={"monospace"}>¡SPACE·R0CKETS!</Heading>
+    </Stack>
+  )
+}
+
+// export default Navbar
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (  
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <VStack className="App" h={"100%"} w={"100%"} >
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </VStack>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 )
